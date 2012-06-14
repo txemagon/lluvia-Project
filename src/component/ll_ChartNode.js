340d903846acc56964582d317a214008f3e725f9
root <root@dai10-desktop.(none)>
2012-06-14 13:33:35 +0200

    ChartNode methods
    Methods added:
      append

--- src/component/ll_ChartNode.js
+++ src/component/ll_ChartNode.js
@@ -1,11 +1,48 @@
 function ChartNode(object){
+
   this.object = object
+  this.children = []
 }
 
-function Children(newchild){
-  this.name = 
-  var myChildren = new Array
-    
-    myChildren.push(newchild)
-   
+ChartNode.to = function(object){
+   if (object instanceof ChartNode)
+    return object
+   return new ChartNode(object)
 }
+
+ChartNode.is_parent$U = function(node){  return node instanceof Array }
+ 
+ChartNode.prototype.insert_children = function (child){
+    this.children.push(child = ChartNode.to(child))  
+    return child
+}
+
+ChartNode.prototype.first = function (){
+    return this.children[0]
+}
+
+ChartNode.prototype.last = function (){
+    return this.children[this.children.length - 1]
+}
+
+
+ChartNode.prototype.append = function(child){
+
+    var new_node
+
+    /* Inserting a whole branch */
+    if (child instanceof Array)
+        for (var i=0; i<child.length; i++)
+            if (ChartNode.is_parent$U(child[i])){
+                new_node = this.insert_children(child[i][0])
+                if (typeof(child[i][1]) !== "undefined" )
+                   new_node.append(child[i][1])
+            }else
+                this.insert_children(child[i])
+
+    /* Inserting a leaf node */
+   else 
+       this.insert_children(child)
+  
+}
+
--- src/component/tests/chart_node_tests.js
+++ src/component/tests/chart_node_tests.js
@@ -17,8 +17,98 @@ assert("ChartNode simple constructor",
       )
 
 assert("ChartNode appends a new ChartNode",
-       "a.children[0].name", "'Junior'",
+       "a.children[0].object.name", "'Junior'",
        "a = new ChartNode({name:'Raul'});\
         b = new ChartNode({name:'Junior'});\
         a.append(b);"
       )
+
+assert("ChartNode appends some different from a ChartNode",
+       "a.children[0].object.name", "'Junior'",
+       "a = new ChartNode({name:'Raul'});\
+        a.append({name:'Junior'});"
+      )
+
+assert("ChartNode appends an Array of ChartNodes",
+       "a.children[1].object.name", "'Padre2'",
+       "a = new ChartNode({name:'Abuelo'});\
+        b = new ChartNode({name:'Padre1'});\
+        c = new ChartNode({name:'Padre2'});\
+        a.append([[b], [c]]);"
+      )
+
+assert("ChartNode appends an Array of ChartNodes",
+       "a.children[1].object.name", "'Padre2'",
+       "a = new ChartNode({name:'Abuelo'});\
+        b = new ChartNode({name:'Padre1'});\
+        c = new ChartNode({name:'Padre2'});\
+        a.append([b, c]);"
+      )
+
+
+assert("ChartNode appends an Array of ChartNodes",
+       "a.children[1].children[0].object.name", "'Hijo1-P2'",
+       "a = new ChartNode({name:'Abuelo'});\
+        b = new ChartNode({name:'Padre1'});\
+        c = new ChartNode({name:'Padre2'});\
+        d = new ChartNode({name:'Hijo1-P1'});\
+        e = new ChartNode({name:'Hijo2-P1'});\
+        f = new ChartNode({name:'Hijo1-P2'});\
+        a.append([[b, [d, e]], [c, [f]]]);"
+      )
+
+assert("ChartNode appends an Array of ChartNodes",
+       "a.children[0].children[1].object.name", "'Hijo2-P1'",
+       "a = new ChartNode({name:'Abuelo'});\
+        b = new ChartNode({name:'Padre1'});\
+        c = new ChartNode({name:'Padre2'});\
+        d = new ChartNode({name:'Hijo1-P1'});\
+        e = new ChartNode({name:'Hijo2-P1'});\
+        f = new ChartNode({name:'Hijo1-P2'});\
+        a.append([[b, [d, e]], [c, [f]]]);"
+      )
+
+assert("ChartNode appends an Array of whatever",
+       "a.children[0].children[1].object.name", "'Hijo2-P1'",
+       "a = new ChartNode({name:'Abuelo'});\
+        a.append([[{name:'Padre1'}, [{name:'Hijo1-P1'}, {name:'Hijo2-P1'}]], [{name:'Padre2'}, [{name:'Hijo1-P2'}]]]);"
+      )
+
+given("Given a bunch of branches.",
+      $LLGvn = "a = new ChartNode({name:'Abuelo'});\
+        b = new ChartNode({name:'Padre1'});\
+        c = new ChartNode({name:'Padre2'});\
+        d = new ChartNode({name:'Hijo1-P1'});\
+        e = new ChartNode({name:'Hijo2-P1'});\
+        f = new ChartNode({name:'Hijo1-P2'});\
+        a.append([[b, [d, e]], [c, [f]]]);" )
+eval($LLGvn)
+
+assert("ChartNode#first",
+       "first.object.name", "'Padre1'",
+       "first = a.first()" )
+
+assert("ChartNode#last",
+       "last.object.name", "'Padre2'",
+       "last = a.last()" )
+
+assert("ChartNode#find"
+       "child", "b"
+       "child = a.find(b)")
+
+assert("ChartNode#has$U"
+       "a.has$U(e)", "false"
+       ";")
+
+assert("ChartNode#has$U"
+       "a.has$U(e, true)", "true"
+       ";")
+
+assert("ChartNode#detach"
+       "a.has$U(b)", "false"
+       "a.detach(b)")
+
+assert("ChartNode#first and ChartNode#last",
+       "a.first.object.name", "'Padre2'",
+       "a.first(a.last())" )
+
