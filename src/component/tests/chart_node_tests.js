6fbfbb400b02d8f0793a23d8457edf15385c2179
txemagon / imasen <txema.gonz@gmail.com>
2012-06-18 15:29:40 +0200

    chahc

--- src/component/tests/chart_node_tests.js
+++ src/component/tests/chart_node_tests.js
@@ -2,7 +2,6 @@ assert("Scaffolding working",
        "a", "a",
        "a=2;")
 
-
 assert("ChartNode class exists",
        "a", "0",
        "try {a = 0; \
@@ -13,12 +12,12 @@ assert("ChartNode class exists",
 
 assert("ChartNode simple constructor",
        "a.object.name", "'Raul'",
-       "a = new ChartNode({name:'Raul'});"
-      )
+       "a = new ChartNode({name:'Raul'});" )
 
 assert("ChartNode appends a new ChartNode",
        "a.children[0].name", "'Junior'",
        "a = new ChartNode({name:'Raul'});\
         b = new ChartNode({name:'Junior'});\
-        a.append(b);"
-      )
+        a.append(b);")
+
+ass
