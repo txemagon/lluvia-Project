12f5e2973825a3bcac7fea4f5cbb1560c500dac9
txemagon / imasen <txema.gonz@gmail.com>
2014-04-02 23:17:11 +0200

    getting lluvia demo to run

--- vendor/demo/javascript/application.js
+++ vendor/demo/javascript/application.js
@@ -4,17 +4,16 @@
  */
 
 
- function main(){	
- 	var vm = document.getElementById("ventanaMascara")
-	vm.style.width = screen.width + "px"
-	var apps = document.getElementsByClassName("Aplicacion")
-	document.getElementById("fondo").style.width = (apps.length * (screen.availWidth + 10)) + "px"
-	for (var i=0; i<apps.length; i++)
+function main(){	
+		var vm = document.getElementById("ventanaMascara")
+		vm.style.width = screen.width + "px"
+		var apps = document.getElementsByClassName("Aplicacion")
+		document.getElementById("fondo").style.width = (apps.length * (screen.availWidth + 10)) + "px"
+		for (var i=0; i<apps.length; i++)
 		apps[i].style.width = screen.availWidth + "px"
-	//document.getElementById("ventanaMascara").style.length = creen.availWidth + "px"
-	var pc   = new ControlPanel_App("panelControl")
-	var pant = new Fondo_App("fondo")
-	pc.addPort("go_to", pant)
- }
+		//document.getElementById("ventanaMascara").style.length = creen.availWidth + "px"
+		var pc   = new ControlPanel_App("panelControl")
+		var pant = new Fondo_App("fondo")
+		pc.addPort("go_to", pant)
+}
 
-main()
