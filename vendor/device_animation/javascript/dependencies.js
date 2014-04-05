0829926a2bc99893e78c011bfa00e4ed776faf9a
Jose <jose@jose-VirtualBox.(none)>
2014-04-05 20:37:03 +0200

    Added interactive demonstration on devices, initial idea.

---
+++ vendor/device_animation/javascript/dependencies.js
@@ -0,0 +1,10 @@
+$K_app_dependencies = [
+	{
+	    module: "Devices",
+	    description: "Interactive demonstration on devices",
+	    path: "",
+	    files: [
+            {name: "main.js", description: "main function"}
+	    ]
+}
+]
