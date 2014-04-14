afa2deebd499d15869aadc757b12acdfbc85a946
mlosada <mlosada@ubuntu.(none)>
2014-04-14 06:20:35 -0700

    timer and some menu changes

--- vendor/pigs&sheeps/javascript/menu_animation.js
+++ vendor/pigs&sheeps/javascript/menu_animation.js
@@ -5,3 +5,33 @@ function change_image(){
 function first_image(){
     document.getElementById('menu_img').src="images/sheep1.png"
     }
+
+function countdown(){
+	sec =0;
+	min =2;
+	seconds = document.getElementById("seconds");
+	minutes = document.getElementById("minutes");
+	 
+	timer = setInterval(
+	    function(){
+	    if(min >=0 && sec<=59){
+	     	if(sec==0){
+	            sec=60;
+	            min--;
+	            minutes.innerHTML = min;
+
+	        }
+	        if(min==0 && sec<10){
+	        	seconds.style.color="red";
+	        	minutes.style.color="red";
+	        	seconds.innerHTML =  "0" + sec;
+	        	sec--;
+	        }
+	        else{	 
+	        	seconds.innerHTML =  sec;
+	        	sec--;	
+	    	}
+	     }
+	    }
+	    ,1500);
+}
