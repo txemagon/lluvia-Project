e45a30b843dbcb916d9060797dc4f9263efaf8bf
lau-zorrilla <laura.zfer@gmail.com>
2014-04-15 12:31:44 +0200

    Virtual methods on class Line done

--- src/Mathematics/Geometry/shape/ll_Line.js
+++ src/Mathematics/Geometry/shape/ll_Line.js
@@ -2,3 +2,48 @@
  * @class       Line
  * Define method for lines and curves
  */
+
+function Line() {
+	const ERROR = 0.00001
+}
+
+Line.prototype.get_initial_point = function() {
+	throw "virtual function invocation: Please define get_initial_point():Vector"
+}
+
+Line.prototype.get_final_point = function() {
+	throw "virtual function invocation: Please define get_final_point():Vector"
+}
+
+Line.prototype.at = function() {
+	throw "virtual function invocation: Please define at(lambda:Number):Vector"
+}
+
+Line.prototype.get_arc_length = function() {
+	//Note: lambda = lambda || 1
+	throw "virtual function invocation: Please define get_arc_length([lambda:Number=1]):Number"
+}
+
+Line.prototype.get_tangent = function() {
+	throw "virtual function invocation: Please define get_tangent(lambda:Number):Vector"
+}
+
+Line.prototype.get_normal = function() {
+	throw "virtual function invocation: Please define get_normal(lambda:Number):Vector"
+}
+
+Line.prototype.get_perpendicular = function() {
+	throw "virtual function invocation: Please define get_perpendicular([point:Vector | iterator:Number]):StraightLine"
+}
+
+Line.prototype.belongs_to = function() {
+	throw "virtual function invocation: Please define belongs_to(point:Vector):Boolean"
+}
+
+Line.prototype.intersects = function() {
+	throw "virtual function invocation: Please define intersects(line:Line):Boolean"
+}
+
+Line.prototype.get_intersection = function() {
+	throw "virtual function invocation: Please define get_intersection(line:Line):Vector"
+}
