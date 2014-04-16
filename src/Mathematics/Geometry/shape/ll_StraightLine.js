7aac4fb6e26377d567a3b09b605f1678a0b5cfd9
lau-zorrilla <laura.zfer@gmail.com>
2014-04-16 18:01:41 +0200

    StraightLine final fixes

--- src/Mathematics/Geometry/shape/ll_StraightLine.js
+++ src/Mathematics/Geometry/shape/ll_StraightLine.js
@@ -10,41 +10,37 @@
  function StraightLine(initial_point, final_point) {
  	this.initial point = initial_point
  	var that = this
- 	this.director = new Vector( final_point.subs( new Vector(initial_point) ) )
+ 	this.director = new Vector( final_point.subs( new Vector(this.initial_point) ) )
 
  }
 
  StraightLine.prototype.get_initial_point = function(){
 
- 	return initial_point
+ 	return this.initial_point
  }
 
  StraightLine.prototype.get_final_point = function(){
 
- 	return new Vector( this.initial_point.add(this.director))
+ 	return new Vector( this.initial_point.add(this.director) )
  }
 
-  StraightLine.prototype.at = function(lambda){
+ StraightLine.prototype.at = function(lambda){
 
-  	var x = this.director.get_coord(0) * lambda
-  	var y = this.director.get_coord(1) * lambda  	
-
- 	return director(x, y).subs( initial_point )
+  	return intial_point.add( this.director.scale(lamda) )
  }
 
  StraightLine.prototype.get_tangent = function(lambda){
- 	var lambda = lambda || 0.1 
 
- 	return director.unit()
+ 	return this.director.unit()
  }
 
  StraightLine.prototype.get_normal = function(lambda){
- 	var lambda = lambda || 0.1
+
  	var t = this.get_tangent()
 
- 	var x = this.t.get_coord(0)
- 	var y = this.t.get_coord(1)
+ 	var x = t.get_coord(0)
+ 	var y = t.get_coord(1)
  
- 	return director(-y, x)
+ 	return new Vector(-y, x)
 
  }
