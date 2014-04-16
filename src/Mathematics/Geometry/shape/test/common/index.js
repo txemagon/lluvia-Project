7f744b416ba23403d0a5a63568528c102dc13f11
lau-zorrilla <laura.zfer@gmail.com>
2014-04-16 20:16:07 +0200

    index.js added to line and straight line tests

---
+++ src/Mathematics/Geometry/shape/test/common/index.js
@@ -0,0 +1,13 @@
+var _test_index = [ "../../../../kernel/ll_Object.js",
+                    "../../../../kernel/ll_Array.js", 
+                    "../../../../kernel/ll_Object.js",
+                    "../../../kernel/ll_Module.js",
+                    "../../../../kernel/ll_String.js",
+                    "../../../../kernel/ll_Function.js",
+                    "../../../../kernel/ll_Exception.js",
+                    "../../../ll_Vector.js",
+                    "../../ll_Line.js",
+                    "../../ll_StraightLine.js",
+                "../line_test.js",
+                "../straight_line_test.js"
+                 ]
