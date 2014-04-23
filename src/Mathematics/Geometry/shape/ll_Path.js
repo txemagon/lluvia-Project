85063a7439be40ba7cb9342c28ba685661caf93c
txema <txema@txema-VirtualBox.(none)>
2014-04-23 11:32:16 +0200

    Path#update_summary

--- src/Mathematics/Geometry/shape/ll_Path.js
+++ src/Mathematics/Geometry/shape/ll_Path.js
@@ -1,7 +1,14 @@
 /**
 * @Class Path
+*
+* Line composed by a set of lines.
+*
+* @constructor Path
 * 
 * @param {Array} lines Array of lines that form the path
+* @property {Array} summary Internal variable for mapping lambdas.
+* @property {Number} total_length Total length of the line.
+*
 */
 
 Path.prototype = new Array
@@ -14,28 +21,49 @@ function Path(lines){
 }
 
 /**
+ * @method update_summary
+ *
+ * Paths a are composed by one or more lines. Thus it maps global
+ * lambdas into line local ones. summary is an internal property to
+ * perform this task fluently.
+ * 
+ * Every time a Line is added or removed this property ought to be updated.
+ */
+Path.prototype.update_summary = function() {
+   this.total_length = 0
+
+   /* Calculate segments lengths */
+   for (var i=0; i<this.length; i++){
+      var new_length = this[i].get_arc_length()
+      this.summary[i] = { arc_length: new_length }
+      this.total_length += new_length
+   }
+
+   /* Map lambdas*/
+   var acumulated_length = 0
+   for (var i=0; i<this.length; i++){
+      acumulated_length += this.summary[i].arc_length
+      this.summary[i].range = {
+         lambda0: i ? this.summary[i-1].range.lambda1 : 0,
+         lambda1: acumulated_length / this.total_length
+      }
+   }
+}
+
+/**
  * @method push
  * Adds a new Line to the Path. Updates internal parameters (as length).
  *
  */
 Path.prototype.push = function(){
-	for (var i=0; i<arguments.length; i++){
+   for (var i=0; i<arguments.length; i++){
 
-	  if (!(arguments[i] instanceof Line))
-		  throw "Path Error. Only lines can be pushed to paths. " + arguments[i].toSource()
+      if (!(arguments[i] instanceof Line))
+         throw "Path Error. Only lines can be pushed to paths. " + arguments[i].toSource()
 
-	  var new_length = arguments[i].get_arc_length()
-	  this.total_length += new_length
-	  this.summary[this.length] = { 
-		  arc_length: new_length,
-		  range: {
-			  lambda0: this.length ? this.summary[this.length-1].range.lambda1 : 0,
-			  lambda1: new_length / this.total_length
-		  } 
-	  }
-
-	  Array.prototype.push.call(this, arguments[i])
-	}
+      this.update_summary()
+      Array.prototype.push.call(this, arguments[i])
+   }
 }
 
 /**
@@ -44,21 +72,21 @@ Path.prototype.push = function(){
  */
 Path.prototype.lambda_to_line = function(path_lambda){
 
-	if (path_lambda < 0 || path_lambda > 1)
-		throw "Invalid lambda: " + path_lambda + ". Must be in [0,1]."
+   if (path_lambda < 0 || path_lambda > 1)
+      throw "Invalid lambda: " + path_lambda + ". Must be in [0,1]."
 
-	var locator = {
-		line: 0,
-	    lambda: 0	
-	}
+   var locator = {
+      line: 0,
+      lambda: 0	
+   }
 
-	for (var i=0; i<this.length; i++)
-	  if (this.summary[i].range.lambda1 < path_lambda)
-		  locator.line = i
+   for (var i=0; i<this.length; i++)
+   if (this.summary[i].range.lambda1 < path_lambda)
+      locator.line = i
 
-	  locator.lambda = ( path_lambda - this.summary[locator[line]].range.lambda0 ) / (this.summary[locator[line]].range.lambda1 - this.summary[locator[line]].range.lambda0)
+   locator.lambda = ( path_lambda - this.summary[locator[line]].range.lambda0 ) / (this.summary[locator[line]].range.lambda1 - this.summary[locator[line]].range.lambda0)
 
-	return locator
+   return locator
 }
 /**
  * @method at
@@ -71,10 +99,10 @@ Path.prototype.lambda_to_line = function(path_lambda){
  * @return {}    
  */
 Path.prototype.at = function(lambda){
-	lambda = lambda || 1
-	var local = lambda_to_line(lambda)
+   lambda = lambda || 1
+   var local = lambda_to_line(lambda)
 
-	return this[local.line].at(local.lambda)
+   return this[local.line].at(local.lambda)
 }
 
 
