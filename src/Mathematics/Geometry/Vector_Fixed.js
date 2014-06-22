16b30c53a968d082c97cb1cbefa0870cd890a7f4
lau-zorrilla <laura.zfer@gmail.com>
2014-06-22 22:05:05 +0200

    Fixed vector class can add and susbstract. Vector accepts different dimension vectors

--- src/Mathematics/Geometry/Vector_Fixed.js
+++ src/Mathematics/Geometry/Vector_Fixed.js
@@ -1,56 +1,106 @@
 /**
- * @classDescription Creates a fixed vector.
- * 
- * @param  {foot} 
- * @param  {free_vector} 	
- * @return {Vector} 	
- * @constructor 
+ * @class FixedVector
+ * Creates a vector fixed in a coordinates axis.
+ *
+ * @param  {foot}
+ * @param  {free_vector}
+ * @return {Vector}
+ * @constructor
  */
 
 FixedVector.prototype = new Vector
 FixedVector.prototype.constructor = FixedVector
 FixedVector.super = Vector
 
-	
-function FixedVector(foot, free_vector){
-	this.foot = foot || new Vector(0, 0)
-	this.free_vector = free_vector || new Vector(0, 0)
 
-	this.fixed_vector = this.foot.add(this.free_vector)
+function FixedVector(free_vector, foot) {
 
-	Vector.call(this, this.foot)
-	Vector.call(this, this.free_vector)
+    this.free_vector = free_vector || new Vector(0, 0)
+    this.foot = foot || new Vector(0, 0)
+
+    if (this.free_vector instanceof Array)
+        this.free_vector = new Vector(free_vector)
+
+    if (this.foot instanceof Array)
+        this.foot = new Vector(foot)
+
+    this.foot.standarize_coordinates(this.free_vector)
+
+    Vector.call(this, this.free_vector)
+    Vector.call(this, this.foot)
 
     Object.defineProperty(this, "_head", {
-    	value: this.fixed_vector,
-	    enumerable: false
+        value: this.foot.add(this.free_vector),
+        enumerable: false
     })
 
-    // Object.defineProperty(this, "head", {
-    // 	get: function(){ return this._head }
-    // })
-}	
-
-/**
- * 
- * @return {[type]} [description]
- */
-
-FixedVector.prototype.get_free_vector = function() {
-	return this.free_vector
 }
 
-    // Object.defineProperty(this, "path", {
-    //     get: function(){ return this._path },
-    //     set: function(path){
-    //         this._path = path 
-    //         if (this._path[this._path.length - 1] != "/")
-    //             this._path += "/"
-    //         this._path = this._path.replace(/\/+/g, "/")
-    //     }
-    // })
+/**
+ * Gives the free vector of the given one
+ *
+ * @return {Vector} Returns the free vector
+ */
 
-    // Object.defineProperty(this, "filepath", {
-    //      value: filepath,
-    //      enumerable: false
-    // })
+FixedVector.prototype.freed = function() {
+    return this.free_vector
+}
+
+/**
+ * Tells if a vector is equal to the given one
+ *
+ * @return {Vector} Returns true if vectors are equal and false if not
+ */
+FixedVector.prototype.equals_to$U = function(vector_to_compare) {
+    if (this.foot.eql$U(vector_to_compare.foot) && +this.free_vector.eql$U(vector_to_compare.free_vector))
+        return true
+    return false
+}
+
+/**
+ * Adds one vector to another as long as the first's head equals the latter's foot
+ *
+ * @return {}
+ */
+
+FixedVector.prototype.add = function(vec) {
+    if (this._head.eql$U(vec.foot)) {
+        var new_free = this.free_vector.add(vec.free_vector)
+
+        return new FixedVector(new_free, this.foot)
+
+    } else
+        throw ("Invalid operation for fixed vectors." +
+            " First vector head must be equal to second's foot")
+}
+
+/**
+ *
+ *
+ * @return {}
+ */
+FixedVector.prototype.add$B = function(vectAdd) {
+    var vectRes = FixedVector.prototype.add.apply(this, arguments)
+    this.cloneCoords(vectRes)
+
+    //return vectRes
+
+    var c = new Vector(vectRes)
+
+    c.foot = vec.foot.add(vec)
+
+    return new FixedVector(c, c.foot)
+}
+
+/**
+ *
+ *
+ * @return {}
+ */
+
+FixedVector.prototype.subs = function(vec) {
+    var new_free = this._head.subs(vec._head)
+    var new_foot = vec._head
+
+    return new FixedVector(new_free, new_foot)
+}
--- src/Mathematics/Geometry/tests/fixed_vector_test.js
+++ src/Mathematics/Geometry/tests/fixed_vector_test.js
@@ -1,30 +1,80 @@
 alert("cargaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, borbabor")
-assert( "Fixed Vector can create an instance of itself.",
-        'b', 'true',
-        'var a = new FixedVector(); \
+assert("Fixed Vector can create an instance of itself.",
+    'b', 'true',
+    'var a = new FixedVector(); \
         if(a instanceof FixedVector) \
-        	var b = true' )
+            var b = true')
 
-assert( "Fixed Vector can create an instance of itself 2.",
-        'a.toSource()', '"({foot:{Coord:[0, 0], _module:0, uVector:[NaN, NaN]}, free_vector:{Coord:[0, 0], _module:0, uVector:[NaN, NaN]}, fixed_vector:{Coord:[0, 0], _module:0, uVector:[NaN, NaN]}, Coord:[0, 0], _module:0, uVector:[NaN, NaN]})"',
-        'var a = new FixedVector()' )
+assert("Fixed Vector can create an instance of itself 2.",
+    'a.toSource()', '"({free_vector:{Coord:[0, 0], _module:0, uVector:[NaN, NaN]}, foot:{Coord:[0, 0], _module:0, uVector:[NaN, NaN]}, Coord:[0, 0], _module:0, uVector:[NaN, NaN]})"',
+    'var a = new FixedVector()')
 
-assert( "Fixed Vector accepts one parameter.",
-        'a.fixed_vector', '[2,3]',
-        'var a = new FixedVector( new Vector(2,3) )' )
+assert("Fixed Vector accepts one parameter.",
+    'a._head', '[2,3]',
+    'var a = new FixedVector( new Vector(2,3) )')
 
-assert( "Fixed Vector accepts two parameters",
-        'a.fixed_vector', '[6,9]',
-        'var a = new FixedVector( new Vector(2,3), new Vector(4,6) )' )
+assert("Fixed Vector accepts two parameters",
+    'a._head', '[6,9]',
+    'var a = new FixedVector( new Vector(2,3), new Vector(4,6) )')
 
-assert( "Fixed Vector can give the foot vector.",
-        'a.foot', '[2,3]',
-        'var a = new FixedVector(new Vector(2,3), new Vector(4,6) )' )
+assert("Fixed Vector can give the foot vector.",
+    'a.foot', '[4,6]',
+    'var a = new FixedVector(new Vector(2,3), new Vector(4,6) )')
 
-assert( "Fixed Vector can give the free vector.",
-        'a.free_vector', '[4,6]',
-        'var a = new FixedVector(new Vector(2,3), new Vector(4,6) )' )
+assert("Fixed Vector can give the free vector.",
+    'a.free_vector', '[2,3]',
+    'var a = new FixedVector(new Vector(2,3), new Vector(4,6) )')
 
-assert( "Fixed Vector can give the _head vector.",
-        'a._head', '[6,9]',
-        'var a = new FixedVector(new Vector(2,3), new Vector(4,6) )' )
+assert("Fixed Vector can give the _head vector.",
+    'a._head', '[6,9]',
+    'var a = new FixedVector(new Vector(2,3), new Vector(4,6) )')
+
+assert("Fixed Vector accepts vectors with different dimensions.",
+    'a._head', '[5,11,4,7]',
+    'var a = new FixedVector(new Vector(2,3,4,7), new Vector(3,8) )')
+
+assert("Fixed Vector can accept array parameters.",
+    'a._head', '[6,9]',
+    'var a = new FixedVector( [2,3], [4,6] )')
+
+assert("Fixed Vector can accept array parameters with different dimensions.",
+    'a._head', '[6,9,5,8]',
+    'var a = new FixedVector( [2,3,5,8], [4,6] )')
+
+assert("Fixed Vector returns the free vector.",
+    'a.freed()', '[2,3,5,8]',
+    'var a = new FixedVector( [2,3,5,8], [4,6] )')
+
+assert("Fixed Vector can be equal to another.",
+    'a.equals_to$U(b)', 'true',
+    'var a = new FixedVector( [2,3,5,8], [4,6] ); \
+        var b = new FixedVector([2,3,5,8], [4,6] )')
+
+assert("Fixed Vector can be different to another.",
+    'a.equals_to$U(b)', 'false',
+    'var a = new FixedVector( [2,3,5,8], [4,6] ); \
+        var b = new FixedVector([4,6], [2,3,5,8] )')
+
+assert("Fixed Vector can substracts another.",
+    'c.foot', '[4,8]',
+    'var a = new FixedVector( [1,1], [2,6]); \
+        var b = new FixedVector( [1,3], [2,4]); \
+        var c = a.subs(b)')
+
+assert("Fixed Vector can substracts another.",
+    'c', '[4,8]',
+    'var a = new FixedVector( [1,1], [2,6]); \
+        var b = new FixedVector( [1,3], [2,4]); \
+        var c = a.subs(b)')
+
+assert("Fixed Vector can add another when a._head equals to b.foot.",
+    'c', '[2,6]',
+    'var a = new FixedVector( [1,1], [2,6] ); \
+        var b = new FixedVector( [1,1], [3,7] ); \
+        var c = a.add(b)')
+
+assert("Fixed Vector can't add another when a._head doesn't equals to b.foot.",
+    'c', 'false',
+    'var a = new FixedVector( [1,1], [2,6] ); \
+        var b = new FixedVector( [1,1], [3,8] ); \
+        var c = a._head.eql$U(b.foot)')
