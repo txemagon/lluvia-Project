a28138b328d4314d6376b8ea34e2b3c16d7f8c3f
laura <laura@laura-VirtualBox.(none)>
2014-06-27 14:24:36 +0200

    Graphic representation for FixedVector substraction added. FixedVector constructor modified

--- src/Mathematics/Geometry/ll_FixedVector.js
+++ src/Mathematics/Geometry/ll_FixedVector.js
@@ -1,10 +1,7 @@
 /**
  * @class FixedVector
- * Creates a vector fixed in a coordinates axis.
+ * Creates a vector fixed in a coordinate axis.
  *
- * @param  {Array} input Array with a free vector and a foot
- * @return {Vector}
- * @constructor
  */
 
 FixedVector.prototype = new Vector
@@ -16,23 +13,69 @@ FixedVector.super = Vector
  *
  * ### Example
  * 
+ *     Cases of usage:
+ *     
+ *     -Constructor can accept a minimun of 0 parameters and a maximun of 2
+ *     -Constructor accepts either vectors or arrays to define the free_vector and the foot
+ *     -Constructor accepts only the free vector
+ *     -Constructor accepts only the _head (where foot would be 0,0)
+ *     -Constructor doesn't accept only foot (it wouldn't know where vector ends)
+ *     -Constructor accepts free_vector and foot when is_head is not defined or is false and
+ *     accepts _head and foot when is_head is true
+ *     
  *     new FixedVector()
  *     //=> this.foot = (0,0)
- *     //=> this._head = (0,0) 
+ *     //=> this._head = (0,0)
+ *     //=> this.Coord = (0,0)
  *
- *     Cases of usage:
- *     constructor accepts only the free vector
- *     constructor accepts only the _head (foot = (0,0))
- *     constructor doesn't accept only foot (it wouldn't know where it ends)
- *     constructor can accept a minimun of 0 parameters and a maximun of 3
+ *     new FixedVector( [new Vector(3,5)] )
+ *     //=> this.foot = (0,0)
+ *     //=> this._head = (3,5)
+ *     //=> this.Coord = (3,5)
+ *
+ *     new FixedVector([ [new Vector(3,5), new Vector(6,1) ])
+ *     //=> this.foot = (6,1)
+ *     //=> this._head = (9,6)
+ *     //=> this.Coord = (3,5)
+ *
+ *     new FixedVector([ [3,5], new Vector(6,2) ])
+ *     //=> this.foot = (6,2)
+ *     //=> this._head = (9,7)
+ *     //=> this.Coord = (3,5)
+ *
+ *     new FixedVector([ [3,5], [4,2] ])
+ *     //=> this.foot = (4,2)
+ *     //=> this._head = (7,7)
+ *     //=> this.Coord = (3,5)
+ *
+ *     new FixedVector([ [3,1], [5,2] ], true)
+ *     //=> this.foot = (5,2)
+ *     //=> this._head = (8,3)
+ *     //=> this.Coord = (-2,-1)
+ *
+ *     new FixedVector([ new Vector(3,1), [5,2] ], true)
+ *     //=> this.foot = (5,2)
+ *     //=> this._head = (8,3)
+ *     //=> this.Coord = (-2,-1)
+ *     
  *
  * //NOTA: Vector.prototype.add.call(this, this.foot) //suma this a this.foot usando el metodo de vector
+ *
+ * @param  {Array} input Array with a free vector and a foot
+ * @param {Boolean} is_head Indicates if the parameters passed through input are free_vector and foot or
+ *                          _head and foot
+ * 
+ * @return {Vector}
  */
 
 
-function FixedVector(input) {
+function FixedVector(input, is_head) {
+    //input = [input] See how can brackets be remove
+
     if (!input)
         input = []
+
+    is_head = is_head || false
 
     var input_args = []
     for (var i = 0; i < input.length; i++)
@@ -49,13 +92,16 @@ function FixedVector(input) {
 
     this.foot.standarize_coordinates(free_vector)
 
-    Vector.call(this, free_vector)
+    if(is_head){
+        free_vector = free_vector.subs(this.foot)
+    }
 
     Object.defineProperty(this, "_head", {
         value: this.foot.add(free_vector),
         enumerable: false
     })
 
+    Vector.call(this, free_vector)
 }
 
 
@@ -72,9 +118,10 @@ FixedVector.prototype.eql$U = function(vector_to_compare) {
 }
 
 /**
+ * @method add
  * Adds one vector to another/s as long as the former's head equals the latter's foot
  *
- * @param {FixedVector || Array} vec FixedVector or array of them passed as parameter
+ * @param {FixedVector | Array} vec FixedVector or array of them passed as parameter
  * @return {FixedVector} Returns the FixedVector of the addition
  */
 
@@ -106,9 +153,10 @@ FixedVector.prototype.add = function(vec) {
 }
 
 /**
+ * @method subs
  * Substracts one fixed vector from another or group of them
  *
- * @param {FixedVector || Array} vec FixedVector or array of them passed as parameter
+ * @param {FixedVector | Array} vec FixedVector or array of them passed as parameter
  * @return {FixedVector} Returns the FixedVector of the substraction
  */
 
@@ -125,13 +173,14 @@ FixedVector.prototype.subs = function(vec) {
 }
 
 /**
+ * @method scle
  * Scales a FixedVector according to a number
  *
  * @param {Number} number number passed as parameter
  * @return {FixedVector} Returns a scaled FixedVector
  */
 
-FixedVector.prototype.scle = function(number) { //scale from Vector needs to be redifined
+FixedVector.prototype.scle = function(number) { //scale from Vector needs to be redefined
     scalable = new Vector(this.Coord)
     scalable = scalable.scale(number)
     fv_foot = this.foot.scale(number)
@@ -139,19 +188,19 @@ FixedVector.prototype.scle = function(number) { //scale from Vector needs to be 
     return new FixedVector([scalable, fv_foot])
 }
 
-//producto vectorial. See if it is necessary to redifined
-//FixedVector.prototype.cross = function(){}
-
 /**
+ * @method virial
  * 
  *
- * @param {Number} number number passed as parameter
- * @return {FixedVector} Returns a scaled FixedVector
+ * @param {Vector} vec FixedVector to operate with 
+ * @param {Number} angle Angle between the two Fixed vectors
+ * 
+ * @return {FixedVector} Returns the virial of a vector or vectors
  */
-
-FixedVector.prototype.virial = function() {
-    var new_free = new Vector(this.Coord)
-    var virial = new_free.dot(this.foot)
+FixedVector.prototype.virial = function(vec, angle) {
+    var new_free, virial
+    new_free = new Vector(this.Coord)
+    virial = new_free.dot(this.foot)
 
     return virial
 }
