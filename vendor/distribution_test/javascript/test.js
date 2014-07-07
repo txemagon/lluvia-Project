ab98a6094cd6962ea5e59b06670eb3a5e5db32b3
txemagon / imasen <txema.gonz@gmail.com>
2014-07-07 15:53:13 +0200

    Empty test.js

--- vendor/distribution_test/javascript/test.js
+++ vendor/distribution_test/javascript/test.js
@@ -1,8 +1,4 @@
 function test() {
     // Write your tests here.
-    p.create_catalog($K_script_response)
-    PackageManager.drop(p, "mathematics")
-    alert("PK Load")
-    var a = new Angle(0.9)
-    alert(a.toSource())
+
 }
