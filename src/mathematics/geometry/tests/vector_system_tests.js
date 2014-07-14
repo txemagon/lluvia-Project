6fa92d072eefef7651b4a4d79cbc90013de58eb2
laura <laura@laura-VirtualBox.(none)>
2014-07-14 14:29:09 +0200

    Triangle class started. New functionality added to VectorSystem

--- src/mathematics/geometry/tests/vector_system_tests.js
+++ src/mathematics/geometry/tests/vector_system_tests.js
@@ -52,3 +52,24 @@ assert("An Array can be transformed into an array of vectors and create a Vector
 assert("An Array can be transformed into an array of vectors and create a VectorSystem - 3",
     'c.to_a()', '[ [2, 3], [5, 4], [6, 8], [1, 1] ]',
     'var c = new VectorSystem([2,3,5,4,6,8].to_v(), new Vector(1,1))')
+
+assert("Uniq$B",
+    'c.to_a()', '[ [1, 1] ]',
+    'var c = new VectorSystem(new Vector(1,1), new Vector(1,1)); \
+     c.uniq$B()')
+
+assert("Uniq$B",
+    'c.to_a()', '[ [1, 1] ]',
+    'var c = new VectorSystem(new Vector(1,1), new Vector(1,1), new Vector(1,1)); \
+     c.uniq$B()')
+
+assert("Uniq$B",
+    'a.to_a()', '[ [2,3], [1, 6] ]',
+    'var a = new VectorSystem(new Vector(2,3), new Vector(2,3), new Vector(2,3), new Vector(1,6), new Vector(1,6)); \
+     a.uniq$B()')
+
+
+assert("Uniq$B",
+    'b.to_a()', '[ [2, 3], [1, 6], [5, 9], [7, 1] ]',
+    'var a = new VectorSystem(new Vector(2,3), new Vector(1,6), new Vector(2,3), new Vector(2,3), new Vector(1,6), new Vector(7,1)); \
+     var b = a.splice(2, 3, new Vector(5,9))')
