b5bed25e8b2b4df18fc22cbdaf70fe69212b6b17
jose <josemorenocasero@gmail.com>
2014-08-14 16:10:19 +0200

    Dependencies demo_web adecuation a new loader system

--- vendor/demo/javascript/dependencies.js
+++ vendor/demo/javascript/dependencies.js
@@ -1,32 +1,31 @@
-
-$K_app_dependencies = [
-
-	{	module: "people_animations",
-		description: "Data module providing animation features stored in a json file",
-		path: "../images/people/",
-		files: [
-			{name: "animations.json",   description: "People walking, sizes and directions"}
-		]
-	},
-
-	{	module: "DHTMLClasses",
-		description: "Specific DHTML Classes",
-		path: "DHTMLClasses/",
-		files: [
-			{name: "Godman.js",    description: "Building Gate Effects"},
-			{name: "Edificios.js", description: "People device handler"}
-		]
-	},
-
-	{	module: "sunny",
-		description: "External application defined by third party (lluvia client, not lluvia user)",
-		path: "",
-		files: [
-			{name: "Home.js",                description: "Home Page Application Device"},
-			{name: "ll_App.js",              description: "Normal Application Device"},
-			{name: "Fondo.js",               description: "App Device Group"},
-			{name: "ControlPanel.js",        description: "Global Menu Device"},
-			{name: "application.js",         description: "Application boot functions"}
-		]
-	}
-]
+$K_app_dependencies = {
+    "package": "demo_web",
+    "description": ".",
+    "path": "",
+    "files": [{
+        "name": "../images/people/animations.json",
+        "description": "People walking, sizes and directions"
+    }, {
+        "name": "DHTMLClasses/Godman.js",
+        "description": "Building Gate Effects"
+    }, {
+        "name": "DHTMLClasses/Edificios.js",
+        "description": "People device handler"
+    }, {
+        "name": "Home.js",
+        "description": "Home Page Application Device"
+    }, {
+        "name": "ll_App.js",
+        "description": "Normal Application Device"
+    }, {
+        "name": "Fondo.js",
+        "description": "App Device Group"
+    }, {
+        "name": "ControlPanel.js",
+        "description": "Global Menu Device"
+    }, {
+        "name": "application.js",
+        "description": "Application boot functions"
+    }],
+    "requires": []
+}
