2a03066561936d0767ba258f6bc5101abfa29e03
letylo <hbl.leticia@gmail.com>
2015-03-10 14:41:08 +0100

    boids

--- vendor/space_cowboy/javascript/planet/player.js
+++ vendor/space_cowboy/javascript/planet/player.js
@@ -1,15 +1,30 @@
 Player.prototype = new Boid
 Player.prototype.constructor = Player
 
-function Player(){
-	Character.apply(this, arguments)
+function Player(config){
+	Boid.apply(this, arguments)
+	var that = this
+
+	this.life = 10
+	this.damage = game.points.damage
+	this.resistance = game.points.resistance
+	this.speed = game.points.speed
+	this.img = "..images/ships/player/player_sprite1.png"
+
+	if (planet.enemy.data[4].life == 0)
+		this.img = "..images/ships/player/player_sprite2.png"
+
+	if (planet.enemy.data[9].life == 0)
+		this.img = "..images/ships/player/player_sprite3.png"
+
+	if (planet.enemy.data[13].life == 0)
+		this.img = "..images/ships/player/player_sprite4.png"
+
+	if (planet.enemy.data[18].life == 0)
+		this.img = "..images/ships/player/player_sprite5.png"
 }
 
-/* Como recogo los valores elegidos en el point dealer
-Player.data = [ 
-  { life: 10,
-  	damage: 1,
-  	resistance: 0,
-  	speed: 1,
-    img: "..images/ships/ship_enemy1.png"}
- */
+Player.prototype.draw = function(ctx) {
+	alert("soy player")
+}
+
